3a26ff422cea6f11a5efefbea7455b4059cabca9
Lucasvegaa <lucasvegaa@gmail.com>
2022-04-13 01:30:44 -0300

    add method ProductosApi

--- api/productos.js
+++ api/productos.js
@@ -5,23 +5,27 @@ class ProductosApi {
     }
 
     listar(id) {
-        return this.productos[id]
+        return JSON.stringify(this.productos[id])
     }
 
     listarAll() {
-        
+        return JSON.stringify(this.productos)
     }
 
     guardar(prod) {
-        this.productos.push(prod)
+        this.id++
+        const prod2 = { ...prod, id: this.id }
+        this.productos.push(prod2)
     }
 
     actualizar(prod, id) {
-        
+        const indice = this.productos.indexOf(this.productos.find(producto => producto.id == id))
+        this.productos.splice(indice, 1, { ...prod, id: id })
     }
 
     borrar(id) {
-        
+        const indice = this.productos.indexOf(this.productos.find(producto => producto.id == id))
+        this.productos.splice(indice, 1)
     }
 }
 
---
+++ api/test.js
@@ -0,0 +1,5 @@
+import ProductosApi from "./productos";
+
+const bd = new ProductosApi
+
+console.log(bd.listar(2))
